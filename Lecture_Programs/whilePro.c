f562c7df9d2cb5cf4fbb2ab92e8ba55d897cd082
SainathBangera1 <psainathbangera@gmail.com>
2022-07-23 17:22:48 +0530

    Upload whilePro.c and its output

---
+++ Lecture_Programs/whilePro.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+    char op;
+    printf("Please Enter the Operator : ");
+    scanf("%c", &op);
+
+    while (op != 'q')
+    {
+        printf("Please Enter the 1st Operand: ");
+        scanf("%d", &x);
+        printf("Please Enter the 2nd Operand: ");
+        scanf("%d", &y);
+
+        switch (op)
+        {
+        case '+':
+            printf("Addition of %d and %d is  %d", x, y, x + y);
+            break;
+        case '-':
+            printf("Substraction of %d and %d is  %d", x, y, x - y);
+            break;
+        case '/':
+            printf("Division of %d and %d is  %.2f", x, y, x * (1.0) / y);
+            break;
+        case 'x':
+            printf("Multiplication of %d and %d is  %d", x, y, x * y);
+            break;
+        case '%':
+            printf("Modules of %d and %d is  %d", x, y, x % y);
+            break;
+        default:
+            printf("Please Enter a Valid Operator + , x , - , / , %%  OR Quit (q)");
+            break;
+        }
+        getchar();
+        printf("\nPlease Enter the Operator: ");
+        scanf("%c", &op);
+    }
+}
